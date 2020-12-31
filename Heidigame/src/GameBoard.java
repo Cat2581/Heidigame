73f183e41fecead93e865f7e6503984152d35361
heidi <heidi@HDCPC>
2020-12-31 15:26:17 -0800

    commit 6 - edited the images to make them transparent but still need to resize all of them

--- Heidigame/src/GameBoard.java
+++ Heidigame/src/GameBoard.java
@@ -30,14 +30,14 @@ public class GameBoard extends JPanel {
 	void loadImages() {
 		try {
 			images[0] = ImageIO.read(this.getClass().getResourceAsStream("apple.png"));
-			images[1] = ImageIO.read(this.getClass().getResourceAsStream("avocado.jpg"));
-			images[2] = ImageIO.read(this.getClass().getResourceAsStream("Banana.jpg"));
-			images[3] = ImageIO.read(this.getClass().getResourceAsStream("broccoli.jpg"));
-			images[4] = ImageIO.read(this.getClass().getResourceAsStream("cucumber.jpg"));
-			images[5] = ImageIO.read(this.getClass().getResourceAsStream("Mango.jpg"));
-			images[6] = ImageIO.read(this.getClass().getResourceAsStream("orange.jpg"));
-			images[7] = ImageIO.read(this.getClass().getResourceAsStream("Papaya.jpg"));
-			images[8] = ImageIO.read(this.getClass().getResourceAsStream("Watermelon.jpg"));
+			images[1] = ImageIO.read(this.getClass().getResourceAsStream("avocado.png"));
+			images[2] = ImageIO.read(this.getClass().getResourceAsStream("Banana.png"));
+			images[3] = ImageIO.read(this.getClass().getResourceAsStream("broccoli.png"));
+			images[4] = ImageIO.read(this.getClass().getResourceAsStream("cucumber.png"));
+			images[5] = ImageIO.read(this.getClass().getResourceAsStream("Mango.png"));
+			images[6] = ImageIO.read(this.getClass().getResourceAsStream("orange.png"));
+			images[7] = ImageIO.read(this.getClass().getResourceAsStream("Papaya.png"));
+			images[8] = ImageIO.read(this.getClass().getResourceAsStream("Watermelon.png"));
 		} catch (Exception e) {
 
 		}
